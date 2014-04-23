85a0926ebdb5e4ce0b683664572d123db414ab94
Yanis Guenane <yguenane@gmail.com>
2014-04-23 14:30:17 -0400

    Fix lint for atlassian module
    This commit fixes lint for the atlassian module.
    
    It fixed :
    
     * Useless double quoted string
     * File permission format

--- local-modules/atlassian/manifests/init.pp
+++ local-modules/atlassian/manifests/init.pp
@@ -1,12 +1,13 @@
 #
-# Shared part between JIRA and Confluence (and possibly other Atlassian products that we might use later)
+# Shared part between JIRA and Confluence (and possibly other Atlassian
+# products that we might use later)
 #
 class atlassian {
-    file { "/usr/local/bin/atlassian-log-compress.rb":
-        owner   => root,
-        group   => root,
-        mode    => 755,
-        source  => "puppet:///modules/atlassian/atlassian-log-compress.rb"
+    file { '/usr/local/bin/atlassian-log-compress.rb':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        source  => 'puppet:///modules/atlassian/atlassian-log-compress.rb'
     }
 }
 
