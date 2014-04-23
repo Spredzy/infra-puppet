4d93bf4e54620963db02bbbc3c10d280bd6f806d
Yanis Guenane <yguenane@gmail.com>
2014-04-23 15:30:22 -0400

    Fix lint for confluence module
    This commit fixes lint for the confluence module.
    
    It fixed :
    
     * Useless double quoted string
     * Arrow alignment

--- local-modules/confluence/manifests/init.pp
+++ local-modules/confluence/manifests/init.pp
@@ -1,40 +1,41 @@
 #
-# Managing our customizations on our current production Confluence at https://wiki.jenkins-ci.org/
+# Managing our customizations on our current production
+# Confluence at https://wiki.jenkins-ci.org/
 #
 class confluence {
   file {
   [
-    "/srv/wiki/base",
-    "/srv/wiki/base/bin",
-    "/srv/wiki/base/classes",
-    "/srv/wiki/base/conf",
-    "/srv/wiki/base/logs",
-    "/srv/wiki/base/webapps",
-    "/srv/wiki/base/work",
-    "/srv/wiki/base/temp"
+    '/srv/wiki/base',
+    '/srv/wiki/base/bin',
+    '/srv/wiki/base/classes',
+    '/srv/wiki/base/conf',
+    '/srv/wiki/base/logs',
+    '/srv/wiki/base/webapps',
+    '/srv/wiki/base/work',
+    '/srv/wiki/base/temp'
   ]:
-    ensure => "directory",
-    owner => "wiki",
-    group => "wiki",
-    mode => 755
+    ensure => 'directory',
+    owner  => 'wiki',
+    group  => 'wiki',
+    mode   => '0755'
     ;
-  "/srv/wiki/base/bin/setenv.sh":
-    source => "puppet:///modules/confluence/setenv.sh"
+  '/srv/wiki/base/bin/setenv.sh':
+    source => 'puppet:///modules/confluence/setenv.sh'
     ;
-  "/srv/wiki/base/conf/server.xml":
-    source => "puppet:///modules/confluence/server.xml"
+  '/srv/wiki/base/conf/server.xml':
+    source => 'puppet:///modules/confluence/server.xml'
     ;
-  "/srv/wiki/base/conf/web.xml":
-    ensure => "link",
-    target => "../../current/conf/web.xml"
+  '/srv/wiki/base/conf/web.xml':
+    ensure => 'link',
+    target => '../../current/conf/web.xml'
     ;
-  "/srv/wiki/tomcat-manager.war":
-    source => "puppet:///modules/confluence/tomcat-manager.war"
+  '/srv/wiki/tomcat-manager.war':
+    source => 'puppet:///modules/confluence/tomcat-manager.war'
     ;
-  "/etc/init.d/confluence":
-    # TODO: run "update-rc.d confluence defaults" when this file change
-    source => "puppet:///modules/confluence/confluence.init",
-    mode => 755
+  '/etc/init.d/confluence':
+    # TODO: run 'update-rc.d confluence defaults' when this file change
+    source => 'puppet:///modules/confluence/confluence.init',
+    mode   => '0755'
     ;
   }
 }
