29d17ccf7d7082a71369e6b56fc7ecfb93a55433
Yanis Guenane <yguenane@gmail.com>
2014-04-24 08:04:53 -0400

    Fix lint for user-abayer module
    This commit fixes lint for the user-abayer module.
    
    It fixed :
    
      * Useless double quoted string
      * Incorrect indentation

--- local-modules/user-abayer/manifests/init.pp
+++ local-modules/user-abayer/manifests/init.pp
@@ -1,42 +1,43 @@
+# User abayer
 class user-abayer {
     group {
-        "abayer" :
+        'abayer' :
             ensure  => present;
     }
 
     user {
-        "abayer" :
-            gid     => "abayer",
-            groups  => "infraadmin",
-            shell   => "/bin/bash",
-            home    => "/home/abayer",
+        'abayer' :
             ensure  => present,
+            gid     => 'abayer',
+            groups  => 'infraadmin',
+            shell   => '/bin/bash',
+            home    => '/home/abayer',
             require => [
-                        Group["abayer"],
-                        Group["infraadmin"],
-                        ];
+              Group['abayer'],
+              Group['infraadmin'],
+            ];
     }
 
     file {
-        "/home/abayer" :
+        '/home/abayer' :
             ensure      => directory,
-            require     => User["abayer"],
-            owner       => "abayer",
-            group       => "abayer";
-        "/home/abayer/.ssh" :
+            require     => User['abayer'],
+            owner       => 'abayer',
+            group       => 'abayer';
+        '/home/abayer/.ssh' :
             ensure      => directory,
-            require     => File["/home/abayer"],
-            owner       => "abayer",
-            group       => "abayer";
+            require     => File['/home/abayer'],
+            owner       => 'abayer',
+            group       => 'abayer';
     }
 
     ssh_authorized_key {
-        "abayer" :
-            user        => "abayer",
+        'abayer' :
             ensure      => present,
-            require     => File["/home/abayer/.ssh"],
-            key         => "AAAAB3NzaC1yc2EAAAABIwAAAQEA402I3RoTGntFReTPTs5UGO2HkU4UN3PDZ/slALFXRC6qKMhdySzHfIXJTVx8IE7Z/TcBuM411Hy/HwTZFZBihw/B8mD6ubut5py0GUc8sI/Qo7++1qaEjhXg6aLZGqu+USH0aE/fgqzZq1o8YF+HxuN5FhWKsbL3T1ukf387gT6rhuUje4Ch9ko/h40IsIyvpcqVCGo47SfDz+lCT2A0mXp/rtJRYOTGdqLAUcJ1zZNawf7FrxGtphuppgyGYFHT+qq4lRRlgVu6rZrAWWoDPPexGB4XuRrbcgKXZ595WQjpx+zlz6Og5TNX4bvX59MQPKr8cg3Qj842ZfOgPkBvOw==",
-            type        => "rsa",
-            name        => "abayer@abayer-laptop";
+            user        => 'abayer',
+            require     => File['/home/abayer/.ssh'],
+            key         => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA402I3RoTGntFReTPTs5UGO2HkU4UN3PDZ/slALFXRC6qKMhdySzHfIXJTVx8IE7Z/TcBuM411Hy/HwTZFZBihw/B8mD6ubut5py0GUc8sI/Qo7++1qaEjhXg6aLZGqu+USH0aE/fgqzZq1o8YF+HxuN5FhWKsbL3T1ukf387gT6rhuUje4Ch9ko/h40IsIyvpcqVCGo47SfDz+lCT2A0mXp/rtJRYOTGdqLAUcJ1zZNawf7FrxGtphuppgyGYFHT+qq4lRRlgVu6rZrAWWoDPPexGB4XuRrbcgKXZ595WQjpx+zlz6Og5TNX4bvX59MQPKr8cg3Qj842ZfOgPkBvOw==',
+            type        => 'rsa',
+            name        => 'abayer@abayer-laptop';
     }
 }
