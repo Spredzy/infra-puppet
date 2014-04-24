93a1c1f5a44113b3a558f07f3e8e934fbcdf1104
Yanis Guenane <yguenane@gmail.com>
2014-04-24 08:15:31 -0400

    Fix lint for user-kbsingh module
    This commit fixes lint for the user-kbsingh module.
    
    It fixed :
    
     * Useless double quoted string
     * Incorrect indentation

--- local-modules/user-kbsingh/manifests/init.pp
+++ local-modules/user-kbsingh/manifests/init.pp
@@ -1,3 +1,4 @@
+# User kbsingh
 class user-kbsingh {
     group {
         'kbsingh' :
@@ -6,15 +7,15 @@ class user-kbsingh {
 
     user {
         'kbsingh' :
+            ensure  => present,
             gid     => 'kbsingh',
             groups  => 'infraadmin',
             shell   => '/bin/bash',
             home    => '/home/kbsingh',
-            ensure  => present,
             require => [
-                        Group['kbsingh'],
-                        Group['infraadmin'],
-                        ];
+              Group['kbsingh'],
+              Group['infraadmin'],
+            ];
     }
 
     file {
@@ -32,8 +33,8 @@ class user-kbsingh {
 
     ssh_authorized_key {
         'kbsingh' :
-            user        => 'kbsingh',
             ensure      => present,
+            user        => 'kbsingh',
             require     => File['/home/kbsingh/.ssh'],
             key         => 'AAAAB3NzaC1kc3MAAACBAOltOk+pDxG4dhaVF9BZ3EjQhUsuW6lUI2VJSOwiu3OTsSzg/E0H388GYcoeWDUVkjAbyBHHa9TOotPMUL0NlQqscjMmTDBQeSSXA2COv3RVGgE0Ddwlg7ap5cgFpkxloG3kz7rpFo+dRhEbb6+d3BvJHAXj5Mgr5yzgCe3LAAAAFQD0WvoxratLpy/b/B1eOusjFr93hQAAAIEAwwOL7ZIMF/Kye2zHCUz9dFpVX2rJCiYqRPlbFFHpQXMJGIcqT1u77MO2cB+D98wrNi+eSbMJQNW+LOhLHsq1bhuNy+44SWZ9UnL9z1vVLpwKc0IxAQnPiDSNLzKPVAOOjGkI6CfMDYH2uLbRLbOf3QgyJB5GNy1tarSo0i/wHQUAAACAOmZFJj3Y6FrbGNaE9lBbb1X22GlJTke3sYMBbXKVxEUh+7VJCz9SSienKiuFmcE1Ysi+7wWUenaSeQNot8/WL29SJCXp3TokPQL9zvFMj5Hn7nDSKJe0ZvUt0RfrU3j6kcrei5SkseW5QGPZXq1GzhLseSvS+SN44ZDEOU4BzM0=',
             type        => 'ssh-dss',
