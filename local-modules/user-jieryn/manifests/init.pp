425e1904beef6af4b18de792a72cf37c8f51cc33
Yanis Guenane <yguenane@gmail.com>
2014-04-24 08:12:19 -0400

    Fix lint for user-jieryn module
    This commit fixes lint for the user-jieryn module.
    
    It fixed :
    
     * Useless double quoted string
     * Incorrect indentation

--- local-modules/user-jieryn/manifests/init.pp
+++ local-modules/user-jieryn/manifests/init.pp
@@ -1,42 +1,43 @@
+# User jieryn
 class user-jieryn {
     group {
-        "jieryn" :
+        'jieryn' :
             ensure  => present;
     }
 
     user {
-        "jieryn" :
-            gid     => "jieryn",
-            groups  => "infraadmin",
-            shell   => "/bin/bash",
-            home    => "/home/jieryn",
+        'jieryn' :
             ensure  => present,
+            gid     => 'jieryn',
+            groups  => 'infraadmin',
+            shell   => '/bin/bash',
+            home    => '/home/jieryn',
             require => [
-                        Group["jieryn"],
-                        Group["infraadmin"],
-                        ];
+              Group['jieryn'],
+              Group['infraadmin'],
+            ];
     }
 
     file {
-        "/home/jieryn" :
+        '/home/jieryn' :
             ensure      => directory,
-            require     => User["jieryn"],
-            owner       => "jieryn",
-            group       => "jieryn";
-        "/home/jieryn/.ssh" :
+            require     => User['jieryn'],
+            owner       => 'jieryn',
+            group       => 'jieryn';
+        '/home/jieryn/.ssh' :
             ensure      => directory,
-            require     => File["/home/jieryn"],
-            owner       => "jieryn",
-            group       => "jieryn";
+            require     => File['/home/jieryn'],
+            owner       => 'jieryn',
+            group       => 'jieryn';
     }
 
     ssh_authorized_key {
-        "jieryn" :
-            user        => "jieryn",
+        'jieryn' :
             ensure      => present,
-            require     => File["/home/jieryn/.ssh"],
-            key         => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC/aWXsTuSt6b0BrjhIwZQjzE/n3PYlbWYOCYI35aLWJxoYxiLjdvSLmmiv9tMjy5w7q8FEWmTXYtIrrKbjb2Twbkj/iRcGTzQYrd0JoyrX4MNBhXxjYlBZepdoGnbVm1rsrPDI1kVgAfBGxe9LLgTv81NSaTyyTw8gW2rpEMAwjbm3iBWHscsHAI8smQJnO6F3Oy07MAt5notu/LElmtV8m2eKHYp4WyZxarEW+rLtYivHk18QDxc9cEuY6ThZpBM3cWze/AFWEb7KOCG8vGVDEZmU8I5Q8b+dcI/1LeRLwc6GcTrVgCjQoay8bW17Hf7csn2pETHV2orxyq+Yv6WD",
-            type        => "rsa",
-            name        => "jesse@ayn";
+            user        => 'jieryn',
+            require     => File['/home/jieryn/.ssh'],
+            key         => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC/aWXsTuSt6b0BrjhIwZQjzE/n3PYlbWYOCYI35aLWJxoYxiLjdvSLmmiv9tMjy5w7q8FEWmTXYtIrrKbjb2Twbkj/iRcGTzQYrd0JoyrX4MNBhXxjYlBZepdoGnbVm1rsrPDI1kVgAfBGxe9LLgTv81NSaTyyTw8gW2rpEMAwjbm3iBWHscsHAI8smQJnO6F3Oy07MAt5notu/LElmtV8m2eKHYp4WyZxarEW+rLtYivHk18QDxc9cEuY6ThZpBM3cWze/AFWEb7KOCG8vGVDEZmU8I5Q8b+dcI/1LeRLwc6GcTrVgCjQoay8bW17Hf7csn2pETHV2orxyq+Yv6WD',
+            type        => 'rsa',
+            name        => 'jesse@ayn';
     }
 }
