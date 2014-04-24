da8bd32936b3f9914bcc908a7a1fffedc6209a60
Yanis Guenane <yguenane@gmail.com>
2014-04-24 08:01:27 -0400

    Fix lint for updates_jenkins-ci_org module
    This commit fixes lint for the update_jenkins-ci_org module.
    
    It fixed :
    
     * Useless double quoted string

--- local-modules/updates_jenkins-ci_org/manifests/init.pp
+++ local-modules/updates_jenkins-ci_org/manifests/init.pp
@@ -1,22 +1,21 @@
 #
 #   updates.jenkins-ci.org
 #
-
 class updates_jenkins-ci_org {
   include apache2
   include apache2::log-rotation
 
   file {
-    "/var/www/updates.jenkins-ci.org/readme.html" :
+    '/var/www/updates.jenkins-ci.org/readme.html' :
       ensure  => present,
-      owner   => "root",
-      group   => "root",
-      source  => "puppet:///modules/updates_jenkins-ci_org/readme.html";
-    "/var/www/updates.jenkins-ci.org/.htaccess" :
+      owner   => 'root',
+      group   => 'root',
+      source  => 'puppet:///modules/updates_jenkins-ci_org/readme.html';
+    '/var/www/updates.jenkins-ci.org/.htaccess' :
       ensure  => present,
-      owner   => "root",
-      group   => "root",
-      source  => "puppet:///modules/updates_jenkins-ci_org/htaccess";
+      owner   => 'root',
+      group   => 'root',
+      source  => 'puppet:///modules/updates_jenkins-ci_org/htaccess';
   }
 
   apache2::virtualhost {
